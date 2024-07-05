71ed3387fe75aadeea9e3356e43a5d757d0adb3c
Daniel Gonzalez <166732337+Drgonzalez220@users.noreply.github.com>
2024-07-05 14:32:59 -0400

    Update SQL Starting Point.sql

--- SQL Starting Point.sql
+++ SQL Starting Point.sql
@@ -54,7 +54,7 @@ SELECT *
 FROM employee_demographics
 ORDER BY gender, age DESC;
 
-# The order of the ORDER BY is important, if we use the age first, it won't order by gender because of the unique values of age
+# The order of the ORDER BY is important, if we use the age first
 
 -- HAVING vs WHERE
 
@@ -62,3 +62,30 @@ SELECT gender, AVG(age)
 FROM employee_demographics
 GROUP BY gender
 HAVING AVG(age) > 40;
+
+SELECT occupation, AVG (salary)
+FROM employee_salary
+WHERE occupation LIKE '%manager%'
+GROUP BY occupation
+HAVING AVG(salary) > 50000;
+
+-- LIMIT 
+
+SELECT *
+FROM employee_demographics
+ORDER BY age DESC
+LIMIT 3
+;
+
+SELECT *
+FROM employee_demographics
+ORDER BY age DESC
+LIMIT 2, 1
+;
+
+-- ALIAS: Change the name of the columns
+
+SELECT gender, AVG(age) AS avg_age
+FROM employee_demographics
+GROUP BY gender
+HAVING avg_age > 40;
